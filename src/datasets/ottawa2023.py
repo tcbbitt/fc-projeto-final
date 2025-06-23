a498d34ef973873f59694909e1c2a7626bba1a34
Thiago Bittencourt <tth.bitt@gmail.com>
2025-06-23 01:59:46 -0300

    added docstring

--- src/datasets/ottawa2023.py
+++ src/datasets/ottawa2023.py
@@ -5,6 +5,58 @@ import os
 import torch
 
 class Ottawa2023Dataset(torch.utils.data.Dataset):
+    """
+    A PyTorch Dataset class for loading and processing vibration signals from the Ottawa 2023 dataset.
+
+    Parameters
+    ----------
+    faulty_type : str, optional
+        The type of fault to use as the label. One of: 'inner', 'outer', 'ball', 'cage'. Default is 'inner'.
+    normalize : bool, optional
+        Whether to normalize the waveform signals. Default is False.
+    sample_length : float, optional
+        The length of each sample in seconds. Default is 1.
+    random_sample : bool, optional
+        Whether to randomly sample a window from the signal. Default is False.
+    random_gain : bool, optional
+        Whether to apply a random gain to the waveform signals. Default is False.
+
+    Attributes
+    ----------
+    ottawa2023 : pandas.DataFrame
+        The loaded Ottawa 2023 dataset.
+    faulty_type : str
+        The type of fault used as the label.
+    normalize : bool
+        Whether waveform signals are normalized.
+    sample_length : float
+        The length of each sample in seconds.
+    random_sample : bool
+        Whether random sampling is applied to the signal windows.
+    random_gain : bool
+        Whether random gain is applied to the waveform signals.
+    fs : float
+        The sampling frequency of the signals.
+    samples_per_window : int
+        The number of samples in each window.
+    windows_per_signal : int
+        The number of windows per signal.
+
+    Methods
+    -------
+    __len__()
+        Returns the total number of windows in the dataset.
+    __getitem__(idx)
+        Retrieves a waveform and its corresponding label based on the given index.
+
+    Notes
+    -----
+    - The dataset is loaded from a preprocessed pickle file located at
+      '/data/bearing_datasets/ottawa/processed/full_dataset.bz2'.
+    - The vibration signals are sliced into windows based on the specified sample length.
+    - Random sampling and random gain can be applied to the waveform signals for data augmentation.
+    - Normalization is performed by subtracting the mean and dividing by the standard deviation of the waveform.
+    """
     def __init__(self, faulty_type = 'inner', normalize = False, sample_length = 1, random_sample = False, random_gain = False):
         self.ottawa2023 = pd.read_pickle('/data/bearing_datasets/ottawa/processed/full_dataset.bz2')
         self.faulty_type = faulty_type
